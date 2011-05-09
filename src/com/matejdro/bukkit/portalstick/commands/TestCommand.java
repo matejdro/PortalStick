fd55af06b034794025eb424418f0b773ae12bee5
Oliver Woodings <oliverw92@mac.com>
2011-05-09 12:26:52 +0100

    Test command for testing velocities - possible aerial faith plates

---
+++ src/com/matejdro/bukkit/portalstick/commands/TestCommand.java
@@ -0,0 +1,25 @@
+package com.matejdro.bukkit.portalstick.commands;
+
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import com.matejdro.bukkit.portalstick.util.Permission;
+
+public class TestCommand extends BaseCommand {
+
+	public TestCommand() {
+		name = "test";
+		argLength = 0;
+		usage = "<- test stuff";
+	}
+	
+	public boolean execute() {
+		player.setVelocity(new Vector(10,10,0));
+		return true;
+	}
+	
+	public boolean permission(Player player) {
+		return Permission.adminRegions(player);
+	}
+
+}
